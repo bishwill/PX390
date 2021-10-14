9d31a3fbe48d5c18e72cab91e1ee9586fbaec143
Will Bishop <will.bishop9427@gmail.com>
2021-10-14 18:15:11 +0100

    Added happy_meter function for question 2 in assignment

--- assignment_1.c
+++ assignment_1.c
@@ -5,14 +5,14 @@
 
 
 int isprime(long k);
+void happy_meter(int size);
 
 int main(void)
 {
-    for (int k = 1; k <= 20; k++)
-    {
-        printf("%d %d\n", k, isprime(k));
-    }
-    return(0);
+    happy_meter(0);
+    happy_meter(1);
+    happy_meter(5);
+    happy_meter(10);
 }
 
 /*
@@ -20,14 +20,13 @@ A function to check whether a given number is prime or not
 */
 int isprime(long k)
 {
-    long int i;
     if (k == 1 || k == 2)
     {
         return(1);
     }
     else
     {
-        for (i = 2; i < k; i++)
+        for (long int i = 2; i < k; i++)
         {
             if (k % i == 0)
             {
@@ -36,4 +35,14 @@ int isprime(long k)
         }
         return(1);
     }
+}
+
+void happy_meter(int size)
+{
+    printf("%s", "I am happy about this assignment ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%s", ":-) ");
+    }
+    printf("\n");
 }
